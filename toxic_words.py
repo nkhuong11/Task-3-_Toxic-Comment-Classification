3bdc89491528ca9f51e355a144f1a2a9583320a6
nkhuong11 <khuong.nguyen0322@gmail.com>
2018-06-05 11:19:22 +0700

    first commit

---
+++ toxic_words.py
@@ -0,0 +1,62 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+TOXIC_WORDS = set("""
+ahole anus ash0le ash0les asholes ass assmonkey assface 
+assh0le assh0lez asshole asshole assholes assholz asswipe 
+azzhole bassterds bastard bastards bastardz basterds 
+basterdz biatch bitch bitches blowjob boffing bullshit 
+butthole buttwipe c0ck c0cks c0k carpetmuncher cawk cawks 
+clit cnts cntz cock cockhead cockhead cocks cocksucker
+crap cum cunt cunts cuntz dick dild0 dild0s 
+dildo dildos dilld0 dilld0s dominatricks dominatrics 
+dominatrix dyke enema fag fag1t faget fagg1t faggit 
+faggot fagit fags fagz faig faigs fart flipping fuck 
+fucker fuckin fucking fucks fudgepacker fuk fukah fuken 
+fuker fukin fukk fukkah fukken fukker fukkin g00k gay 
+gaybor gayboy gaygirl gays gayz goddamned h00r h0ar h0re
+hells hoar hoor hoore jackoff jap japs jerkoff jisim jiss 
+jizm jizz knob knobs knobz kunt kunts kuntz lesbian lezzian 
+lipshits lipshitz masochist masokist massterbait masstrbait 
+masstrbate masterbaiter masterbate masterbates mutha fuker 
+motha fucker fuker fukka fukkah fucka fuchah fukker fukah 
+mothafucker mothafuker mothafukkah mothafukker motherfucker
+motherfukah motherfuker motherfukkah motherfukker 
+motherfucker muthafucker muthafukah muthafuker 
+muthafukkah muthafukker mutha n1gr nastt nasty nigger 
+nigur niiger niigr orafis orgasim orgasm orgasum 
+oriface orifice orifiss packi packie packy paki pakie 
+paky pecker peeenus peeenusss peenus peinus pen1s penas 
+penis penisbreath penus penuus phuc phuck phuk phuker 
+phukker polac polack polak poonani pr1c pr1ck pr1k pusse 
+pussee pussy puuke puuker queer queers queerz qweers 
+qweerz qweir recktum rectum retard sadist scank schlong 
+screwing semen sex sexx sexxx sx sexy sht sh1t sh1ter 
+sh1ts sh1tter sh1tz shit shits shitter shitty shity shitz 
+shyt shyte shytty shyt skanck skank skankee skankey skanks
+skanky slut sluts slutty slutz sonofabitch tit turd 
+va1jina vag1na vagiina vagina vaj1na vajina vullva vulva 
+w0p wh00r wh0re whore xrated xxx bch bitch blowjob clit 
+arschloch fuck shit ass asshole btch b17ch b1tch bastard 
+bich boiolas buceta c0ck cawk chink cipa clits cock cum 
+cunt dildo dirsa ejakulate fatass fcuk fuk fux0r hoer hore 
+jism kawk l3itch l3i+ch lesbian masturbate masterbat 
+masterbat3 motherfucker s.o.b. mofo nazi nigga niggas 
+nigger nutsack phuck pimpis pusse pussy scrotum shemale 
+shi+ shitt slut smut teets tits boobs b00bs teez testical 
+testicle titt w00se jackoff wank whoar whore damn dyke 
+fuck shit @$$ amcik andskota arse assrammer ayir bi7ch 
+bitch bollock breasts buttpirate cabron cazzo chraa 
+chuj cock cunt d4mn daygo dego dick dike dupa dziwka 
+ejackulate ekrem ekto enculer faen fag fanculo fanny 
+feces feg felcher ficken fitt flikker foreskin fotze 
+fu fuk futkretzn gay gook guiena h0r h4x0r hell helvete 
+hoer honkey huevon hui injun jizz kanker kike klootzak 
+kraut knulle kuk kuksuger kurac kurwa kusi kyrpa lesbo 
+mamhoon masturbat merd mibun monkleigh mouliewop muie 
+mulkku muschi nazis nepesaurio nigger orospu paska perse 
+picka pierdol pillu pimmel piss pizda poontsee poop porn 
+p0rn pr0n preteen pula pule puta puto qahbeh queef 
+rautenberg schaffer scheiss schlampe schmuck screw 
+sharmuta sharmute shipal shiz skrib 
+""".split())
